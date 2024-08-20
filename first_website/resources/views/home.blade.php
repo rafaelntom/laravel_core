63a0ed26b9324b7a9f1f503062f50fb2ae64874d
RafaelTomazini <rafaelnt83@gmail.com>
2024-08-20 20:55:30 -0300

    feature: models, factories and seeders

---
+++ first_website/resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/app.css">
+
+</head>
+
+<body>
+    <h1>Home Page</h1>
+    <p>is this working?</p>
+</body>
+
+</html>
